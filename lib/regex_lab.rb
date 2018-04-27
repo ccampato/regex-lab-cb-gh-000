04f2d79fc54914af3b5264e776df5a0126e63f8e
Caio Sanches <ccampato@gmail.com>
2018-04-27 00:01:40 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\b\d{10}\b/)
+  if phone =~ /\b\d{10}\b/
     true
   else
     false
