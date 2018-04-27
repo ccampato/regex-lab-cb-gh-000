2dae5a9a4129a393415306be562b34ec1fdbcd9c
Caio Sanches <ccampato@gmail.com>
2018-04-27 00:27:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\d/).count
+  count = phone.scan(/\d/).count
   if count.length == 10
     true
   else
