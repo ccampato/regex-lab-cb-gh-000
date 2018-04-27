1ebaf2457e8204e7b9db983e74f1018a1bec6dde
Caio Sanches <ccampato@gmail.com>
2018-04-27 00:28:42 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,32 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiou]/i)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\b[A-Z]\w+[.!?,]/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  count = phone.scan(/\d/).count
+  if count == 10
+    true
+  else
+    false
+  end
 end
